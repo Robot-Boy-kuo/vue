a44c36b3425125c56825cd46de9b1906cd4aa9d3
Robot-Boy-kuo <kuo.shengkun@outlook.com>
2022-09-20 10:02:53 +0800

    增加清空按钮，利用vue双向绑定

--- vue-admin-template-master/src/views/edu/teacher/list.vue
+++ vue-admin-template-master/src/views/edu/teacher/list.vue
@@ -1,6 +1,35 @@
 <template>
     <div class="app-container">
-        讲师列表
+       
+
+        <!--查询表单-->
+        <!-- :inline="true"在一行内显示+单向绑定 -->
+        <el-form :inline="true" class="demo-form-inline">
+            <el-form-item>
+                <el-input v-model="teacherQuery.name" placeholder="讲师名" />
+            </el-form-item>
+        
+            <el-form-item>
+                <el-select v-model="teacherQuery.level" clearable placeholder="讲师头衔">
+                    <el-option :value="1" label="高级讲师" />
+                    <el-option :value="2" label="首席讲师" />
+                </el-select>
+            </el-form-item>
+        
+            <el-form-item label="添加时间">
+                <el-date-picker v-model="teacherQuery.begin" type="datetime" placeholder="选择开始时间"
+                    value-format="yyyy-MM-dd HH:mm:ss" default-time="00:00:00" />
+            </el-form-item>
+            <el-form-item>
+                <el-date-picker v-model="teacherQuery.end" type="datetime" placeholder="选择截止时间"
+                    value-format="yyyy-MM-dd HH:mm:ss" default-time="00:00:00" />
+            </el-form-item>
+        
+            <el-button type="primary" icon="el-icon-search" @click="getList()">查询</el-button>
+            <el-button type="default" @click="resetData()">清空</el-button>
+        </el-form>
+
+
         <!-- 表格 -->
         <el-table v-loading="listloading" :data="list" element-loading-text="数据加载中" border fit highlight-current-row>
             
@@ -86,7 +115,16 @@ export default {
                 .catch(error=>{//请求失败
                     console.log(error)
                 })
+        },
+        
+        resetData() {
+            //1.清空条件值
+            //利用双向绑定，清空绑定的对象
+            this.teacherQuery = {}
+            //2.查询所有数据
+            this.getList()
         }
+        
     }
 
 }
