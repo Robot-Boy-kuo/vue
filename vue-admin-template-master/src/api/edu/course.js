9dcd982a499af5c2dc7d12e5025f3bcee78f224c
Robot-Boy-kuo <kuo.shengkun@outlook.com>
2022-09-22 11:28:42 +0800

    实现课程信息添加的基本操作

---
+++ vue-admin-template-master/src/api/edu/course.js
@@ -0,0 +1,13 @@
+import request from '@/utils/request'
+
+export default {
+    //1. 添加课程信息
+    addCourseInfo(courseInfo) {
+        return request({
+            url: `/eduservice/edu-course/addCourceInfo`,
+            method: 'post',
+            data: courseInfo
+        })
+    },
+
+}
